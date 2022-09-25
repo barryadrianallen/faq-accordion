62918728b1bbd6a23b13395d97c6443db4893ecb
barryadrianallen <barryadrianallen@gmail.com>
2022-09-25 12:46:46 +0100

    make labels bold

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 let images = document.querySelectorAll('img');
+let labels = document.querySelectorAll('label');
 
 let invertArrowImage = (index) => {
    images[index].classList.toggle('rotated');
+   labels[index].classList.toggle('bold-text');
 }
 
 
